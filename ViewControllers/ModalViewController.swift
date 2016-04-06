3f53570a4006bf21f842be38d6243b0cf7ea7b78
Jan Mísař <misar.jan@gmail.com>
2016-04-06 18:22:16 +0200

    Alert controller.

--- ViewControllers/ModalViewController.swift
+++ ViewControllers/ModalViewController.swift
@@ -22,9 +22,35 @@ class ModalViewController: UIViewController {
             make.center.equalTo(0)
         }
         dismissButton.addTarget(self, action: #selector(dismissButtonTapped(_:)), forControlEvents: .TouchUpInside)
+
+        let alertButton = UIButton()
+        alertButton.setTitle("Alert", forState: .Normal)
+        view.addSubview(alertButton)
+        alertButton.snp_makeConstraints { make in
+            make.top.equalTo(dismissButton.snp_bottom)
+            make.centerX.equalTo(0)
+        }
+        alertButton.addTarget(self, action: #selector(alertButtonTapped(_:)), forControlEvents: .TouchUpInside)
     }
 
     func dismissButtonTapped(sender: UIButton) {
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func alertButtonTapped(sender: UIButton) {
+
+        let alertController = UIAlertController(title: "Default Style", message: "A standard alert.", preferredStyle: .Alert)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            print("cancel")
+        }
+        alertController.addAction(cancelAction)
+
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            print("ok")
+        }
+        alertController.addAction(OKAction)
+
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 }
