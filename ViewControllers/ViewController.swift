8d6a852e23343ba82f309147dd3d6b17e477d080
Jan Mísař <misar.jan@gmail.com>
2016-04-06 17:07:16 +0200

    Initial commit.

---
+++ ViewControllers/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ViewControllers
+//
+//  Created by Jan Mísař on 06.04.16.
+//  Copyright © 2016 Ackee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+}
