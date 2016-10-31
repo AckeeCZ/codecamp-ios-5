ddbd87f5ab9d70c10da22104765e5512f84e7140
Jan Misar <misar.jan@gmail.com>
2016-10-31 20:56:07 +0100

    New swift 3 project.

--- ViewControllers/ViewController.swift
+++ ViewControllers/ViewController.swift
@@ -2,45 +2,24 @@
 //  ViewController.swift
 //  ViewControllers
 //
-//  Created by Jan Mísař on 06.04.16.
-//  Copyright © 2016 Ackee. All rights reserved.
+//  Created by Jan Misar on 31.10.16.
+//  Copyright © 2016 Jan Misar. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        // fatalError("init(coder:) has not been implemented")
-    }
-
-    override func loadView() {
-        super.loadView()
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-    }
 
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
 }
+
